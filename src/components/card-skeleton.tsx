cce6dff802c22057a277a0e1d710c4b7d7756f9f
aakash19here <aakash@Aakashs-MacBook-Air.local>
2023-09-30 16:06:44 +0530

    style: skeleton to match card

--- src/components/card-skeleton.tsx
+++ src/components/card-skeleton.tsx
@@ -1,12 +1,12 @@
 import { Skeleton } from "./skeleton";
 
 export default function CardSkeleton() {
-  const SKELETONS = Array.from({ length: 10 }, (_, i) => i + 1);
+  const SKELETONS = Array.from({ length: 15 }, (_, i) => i + 1);
 
   return (
     <div className="grid grid-col-1 md:grid-cols-3 gap-5">
       {SKELETONS?.map((id) => (
-        <Skeleton key={id} className="w-[300px] h-[150px] bg-gray-600" />
+        <Skeleton key={id} className="w-[200px] h-[150px] bg-gray-600" />
       ))}
     </div>
   );
