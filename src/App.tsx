07176c1b6874ac9442b88d899a4ff2450c953744
aakash19here <aakash@Aakashs-MacBook-Air.local>
2023-09-30 16:07:02 +0530

    chore : fix funtion return type

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   CardFooter,
   CardTitle,
 } from "./components/card";
+import CardSkeleton from "./components/card-skeleton";
 
 type Data = {
   count: number;
@@ -32,7 +33,9 @@ type Results = {
   }[];
 }[];
 
-const fetchPokemon = async () => {
+type FetchResult = { data: Data; results: Results };
+
+const fetchPokemon = async (): Promise<FetchResult> => {
   const response = await fetch(
     "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
   );
@@ -50,7 +53,7 @@ const fetchPokemon = async () => {
 
   console.log("[results]", results);
 
-  return results as Results;
+  return { data, results };
 };
 
 export default function App() {
@@ -59,13 +62,12 @@ export default function App() {
     queryKey: ["Pokemon"],
   });
 
-  console.log("hmmmm", data);
-
-  const DisplayPokemon = () => (
+  return (
     <>
+      {isLoading && <CardSkeleton />}
       <div className="grid grid-col-1 md:grid-cols-3 gap-5">
         {!isLoading &&
-          data?.map((pokemon) => (
+          data?.results.map((pokemon) => (
             <Card key={pokemon.id}>
               <CardTitle className="capitalize">{pokemon.name}</CardTitle>
               <CardDescription className="flex justify-center">
@@ -75,14 +77,11 @@ export default function App() {
                 />
               </CardDescription>
               <CardFooter className="capitalize">
-                Type:{" "}
-                {pokemon.types.map((type: any) => type.type.name).join(", ")}
+                Type: {pokemon.types.map((type) => type.type.name).join(", ")}
               </CardFooter>
             </Card>
           ))}
       </div>
     </>
   );
-
-  return <div>{data && <DisplayPokemon />}</div>;
 }
