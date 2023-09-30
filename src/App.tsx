da57cc839a514afa5cb897af608bcfb477974d9e
aakash19here <aakash@Aakashs-MacBook-Air.local>
2023-09-30 15:50:03 +0530

    chore : add types and rtq dev tool

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,37 @@ import {
   CardTitle,
 } from "./components/card";
 
+type Data = {
+  count: number;
+  next: string;
+  previous: null | string;
+  results: {
+    name: string;
+    url: string;
+  }[];
+};
+
+type Results = {
+  id: number;
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+  types: {
+    slot: number;
+    type: {
+      name: string;
+      url: string;
+    };
+  }[];
+}[];
+
 const fetchPokemon = async () => {
   const response = await fetch(
     "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
   );
 
-  const data = await response.json();
+  const data: Data = await response.json();
 
   const results = [];
 
@@ -23,11 +48,13 @@ const fetchPokemon = async () => {
     results.push(pokemonData);
   }
 
-  return results;
+  console.log("[results]", results);
+
+  return results as Results;
 };
 
 export default function App() {
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryFn: fetchPokemon,
     queryKey: ["Pokemon"],
   });
@@ -35,22 +62,26 @@ export default function App() {
   console.log("hmmmm", data);
 
   const DisplayPokemon = () => (
-    <div className="grid grid-col-1 md:grid-cols-3 gap-5">
-      {data?.map((pokemon: any) => (
-        <Card key={pokemon.id}>
-          <CardTitle className="capitalize">{pokemon.name}</CardTitle>
-          <CardDescription className="flex justify-center">
-            <img
-              className="card-image"
-              src={`${pokemon.sprites["front_default"]}`}
-            />
-          </CardDescription>
-          <CardFooter className="capitalize">
-            Type: {pokemon.types.map((type: any) => type.type.name).join(", ")}
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
+    <>
+      <div className="grid grid-col-1 md:grid-cols-3 gap-5">
+        {!isLoading &&
+          data?.map((pokemon) => (
+            <Card key={pokemon.id}>
+              <CardTitle className="capitalize">{pokemon.name}</CardTitle>
+              <CardDescription className="flex justify-center">
+                <img
+                  className="card-image"
+                  src={`${pokemon.sprites["front_default"]}`}
+                />
+              </CardDescription>
+              <CardFooter className="capitalize">
+                Type:{" "}
+                {pokemon.types.map((type: any) => type.type.name).join(", ")}
+              </CardFooter>
+            </Card>
+          ))}
+      </div>
+    </>
   );
 
   return <div>{data && <DisplayPokemon />}</div>;
